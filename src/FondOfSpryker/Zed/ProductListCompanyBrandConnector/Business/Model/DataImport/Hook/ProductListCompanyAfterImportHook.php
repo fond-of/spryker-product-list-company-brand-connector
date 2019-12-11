c2a3fcd5d654507ff3345a23a3fed6365c4669e5
Jozsef Geng <gengjozsef86@gmail.com>
2019-12-11 12:22:06 +0100

    implement after import Hook

---
+++ src/FondOfSpryker/Zed/ProductListCompanyBrandConnector/Business/Model/DataImport/Hook/ProductListCompanyAfterImportHook.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace FondOfSpryker\Zed\ProductListCompanyBrandConnector\Business\Model\DataImport\Hook;
+
+use FondOfSpryker\Zed\BrandCompany\Business\BrandCompanyFacadeInterface;
+use FondOfSpryker\Zed\ProductList\Business\ProductListFacadeInterface;
+use Generated\Shared\Transfer\CompanyBrandRelationTransfer;
+use Orm\Zed\BrandCompany\Persistence\FosBrandCompanyQuery;
+use Spryker\Zed\DataImport\Business\Model\DataImporterAfterImportInterface;
+
+class ProductListCompanyAfterImportHook implements DataImporterAfterImportInterface
+{
+    /**
+     * @var \FondOfSpryker\Zed\BrandCompany\Business\BrandCompanyFacadeInterface
+     */
+    protected $brandCompanyFacade;
+
+    /**
+     * @var \Spryker\Zed\ProductList\Business\ProductListFacadeInterface
+     */
+    protected $productListFacade;
+
+    /**
+     *
+     * @param \FondOfSpryker\Zed\BrandCompany\Business\BrandCompanyFacadeInterface $brandCompanyFacade
+     * @param \FondOfSpryker\Zed\ProductList\Business\ProductListFacadeInterface $productListFacade
+     */
+    public function __construct(
+        BrandCompanyFacadeInterface $brandCompanyFacade,
+        ProductListFacadeInterface $productListFacade
+    ) {
+        $this->brandCompanyFacade = $brandCompanyFacade;
+        $this->productListFacade = $productListFacade;
+    }
+
+    /**
+     * @return void
+     */
+    public function afterImport(): void
+    {
+        $this->rebuildRelations();
+    }
+
+    /**
+     * @return void
+     */
+    protected function rebuildRelations(): void
+    {
+        $this->deleteCompanyBrandRelations();
+        $this->createCompanyBrandRelations();
+    }
+
+    /**
+     * @return int
+     */
+    protected function deleteCompanyBrandRelations(): int
+    {
+        return FosBrandCompanyQuery::create()->doDeleteAll();
+    }
+
+    /**
+     * @return void
+     */
+    protected function createCompanyBrandRelations()
+    {
+        $productListCollectionTransfer = $this->productListFacade->getAllProductLists();
+
+        if ($productListCollectionTransfer === null) {
+            return null;
+        }
+
+        foreach ($productListCollectionTransfer->getProductLists() as $productListTransfer) {
+            if (count($productListTransfer->getBrandRelation()->getIdBrands()) > 0 &&
+                count($productListTransfer->getProductListCompanyRelation()->getCompanyIds()) > 0
+            ) {
+                $this->saveCompanyBrandRelations(
+                    $productListTransfer->getProductListCompanyRelation()->getCompanyIds(),
+                    $productListTransfer->getBrandRelation()->getIdBrands()
+                );
+            }
+        }
+    }
+
+    /**
+     * @param array $companyIds
+     * @param array $brandIds
+     *
+     * @return void
+     */
+    protected function saveCompanyBrandRelations(array $companyIds, array $brandIds): void
+    {
+        foreach ($companyIds as $idCompany) {
+            $this->brandCompanyFacade->saveCompanyBrandRelation(
+                (new CompanyBrandRelationTransfer())->setIdCompany($idCompany)->setIdBrands($brandIds)
+            );
+        }
+    }
+}
